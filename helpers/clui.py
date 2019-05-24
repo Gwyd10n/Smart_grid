9fe24aefb4682425be38bfd0ccb930a24ec2a4bf
Gwyd10n <gwydion.oostvogel@gmail.com>
2019-05-24 18:59:32 +0200

    sim ann

--- helpers/clui.py
+++ helpers/clui.py
@@ -50,15 +50,18 @@ def show_bounds(grid):
     elif yn(user_in) == '':
         show_bounds(grid)
 
+
 def another():
     print('Another?\n yes: [y], no: [n]')
     user_in = input('> ').lower()
+    command(user_in)
     if yn(user_in):
         clui()
     elif yn(user_in) == '':
         another()
     elif not yn(user_in):
         command('quit')
+
 
 def prompt_plot(path):
     print('Plot solution?\n yes: [y], no: [n]')
@@ -68,6 +71,7 @@ def prompt_plot(path):
         plot(path)
     elif yn(user_in) == '':
         prompt_plot(path)
+
 
 def save(new_grid, district, algorithm):
     print('Would you like to save this configuration?\n(NOTE: can only plot if configuration is saved)\n yes: [y], no: [n]')
@@ -97,7 +101,28 @@ def save(new_grid, district, algorithm):
 
 
 def do_alg(alg, grid):
-    if alg == 'hillclimber' or alg == 'simulated_annealing':
+    algorithms = {0: 'random', 1: 'greedy', 2: 'greedy2'}
+    cooling_schemes = {0: 'linear', 1: 'exponential', 2: 'sigmoidal', 3: 'geman&geman'}
+    if alg == 'hillclimber':
+        print('How many iterations')
+        user_in = input('> ')
+        command(user_in)
+        try:
+            n = int(n)
+        except ValueError:
+            print("Invalid number, negatives and decimals are not allowed")
+            return do_alg(alg, grid)
+        print('Algorithm to create initial solution\nrandom: [0], greedy: [1], greedy2: [2]')
+        user_in = input('> ')
+        try:
+            user_in = int(user_in)
+        except ValueError:
+            print('Invalid number, choose one from list below')
+            return do_alg(alg, grid)
+        if user_in >= 0 and user_in <= 2:
+            new_grid = ALGORITHMS[algorithms[user_in]](grid)
+            return ALGORITHMS[alg](new_grid, n)
+    elif alg == 'simulated_annealing':
         print('How many iterations')
         n = input('> ')
         try:
@@ -107,17 +132,88 @@ def do_alg(alg, grid):
             return do_alg(alg, grid)
         print('Algorithm to create initial solution\nrandom: [0], greedy: [1], greedy2: [2]')
         user_in = input('> ')
-        algorithms = {0: 'random', 1: 'greedy', 2: 'greedy2'}
+        command(user_in)
         try:
             user_in = int(user_in)
         except ValueError:
-            print('Invalid number, choose one from list below')
+            print('Invalid number, choose one from list')
             return do_alg(alg, grid)
-        if user_in < 2 or user_in > 0:
+        if user_in >= 0 and user_in <= 2:
             new_grid = ALGORITHMS[algorithms[user_in]](grid)
-            return ALGORITHMS[alg](new_grid, n)
+            print('Set annealing parameters?\nyes: [y], no: [n]\n(default is linear cooling scheme, with Tstart = 10 and Tend = 1)')
+            user_in = (input('> '))
+            command(user_in)
+            if yn(user_in):
+                # Get start temperature
+                user_in = input('Tstart:\n(must be integer greater than 0)\n> ')
+                command(user_in)
+                try:
+                    Ts = float(user_in)
+                except ValueError:
+                    print('Invalid start temperature')
+                    return do_alg(alg, grid)
+                if Ts < 0:
+                    print('Tstart must be positive number')
+                    return do_alg(alg, grid)
+
+                # Get end temperature
+                user_in = input('Tend:\n(must be integer smaller than Tstart and greater than 0)\n> ')
+                command(user_in)
+
+                try:
+                    Te = float(user_in)
+                except ValueError:
+                    print('Invalid end temperature')
+                    return do_alg(alg, grid)
+                if Te < 0:
+                    print('Tend must be positive number')
+                    return do_alg(alg, grid)
+
+                if Te >= Ts:
+                    print('Invalid end temperature')
+                    return do_alg(alg, grid)
+                print('Select cooling scheme')
+                print(''.join(['{0}{1}'.format(str(key) + ': ', value + '  ') for key, value in cooling_schemes.items()]),
+                      end=' ')
+                user_in = input('\n> ')
+                command(user_in)
+
+                try:
+                    user_in = int(user_in)
+                except ValueError:
+                    print('Invalid number, choose one from list')
+                    return do_alg(alg, grid)
+
+                if not user_in in cooling_schemes:
+                    print('Invalid algorithm, choose one from list')
+                    return do_alg(alg, grid)
+                else:
+                    cooling = cooling_schemes[user_in]
+
+                if cooling == 'geman&geman':
+                    print('Choose d value (value must be greater than 0\n default d = 1')
+                    user_in = input('> ')
+                    command(user_in)
+                    try:
+                        d = int(user_in)
+                    except ValueError:
+                        print('Not a number')
+                        do_alg(alg, grid)
+                    if d < 0:
+                        print('d must be greater than 0')
+                        do_alg(alg, grid)
+                    return ALGORITHMS[alg](new_grid, n, cooling, Ts, Te, d)
+                else:
+                    return ALGORITHMS[alg](new_grid, n)
+
+            elif yn(user_in) == '':
+                return do_alg(alg, grid)
+            elif not yn(user_in):
+                return ALGORITHMS[alg](new_grid, n)
+
     else:
         return ALGORITHMS[alg](grid)
+
 
 def prompt_alg():
     algorithms = {0: 'random', 1: 'greedy', 2: 'greedy2', 3: 'hillclimber', 4: 'simulated_annealing'}
@@ -136,6 +232,7 @@ def prompt_alg():
     else:
         return algorithms[user_in]
 
+
 def back():
     print("Go back?\nyes: [y], no: [n]")
     user_in = input('> ').lower()
@@ -145,6 +242,7 @@ def back():
     elif not yn(user_in):
         command('quit')
 
+
 def yn(u_in):
     u_in = u_in.lower()
     if u_in == 'y':
@@ -153,6 +251,7 @@ def yn(u_in):
         return False
     else:
         return ''
+
 
 def command(user_in):
     user_in = user_in.lower()
@@ -178,6 +277,7 @@ def command(user_in):
         print("Provide filename (without file extension):")
         path = os.path.dirname(__file__).replace("helpers", f"data\\results\\{input('> ')}.csv")
         plot(path)
+
 
 def choose_distr():
     print("District to solve:\n[1] [2] [3]")
