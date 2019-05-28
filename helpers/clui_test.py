b4b34e21b1d463ff862fed5bb166a460b6474fd3
Gwyd10n <gwydion.oostvogel@gmail.com>
2019-05-28 21:41:50 +0200

    test clui

--- helpers/clui_test.py
+++ helpers/clui_test.py
@@ -9,7 +9,8 @@ from helpers.greedy2 import greedy2
 from helpers.random_alg import random
 from helpers.hillclimber import hillclimber
 from helpers.sim_annealing import sim_ann
-from helpers.clui import prompt_alg, choose_distr
+from helpers.kmeans import kmeans
+from helpers.helpers import upper_bound, lower_bound, distance
 
 
 ALGORITHMS = {'random': random, 'greedy': greedy, 'greedy2': greedy2,
@@ -24,6 +25,7 @@ def clui_test():
     print('TEST INTERFACE')
     district = choose_distr()
     grid = create_grid(0, 50, 50, district)
+    kmeans(grid)
     alg = prompt_alg()
     result = do_alg(alg, grid)
     path = save(result, district, alg)
@@ -83,11 +85,50 @@ def do_alg(alg, grid):
             for i in range(n_sol):
                 new_grid = ALGORITHMS[algorithms[user_in]](grid)
                 grid = ALGORITHMS[alg](new_grid, n)
-                results.append([i, grid.get_cost()])
+                results.append([i, grid.get_cost(), ])
             return results
     else:
         results = []
         for i in range(n_sol):
             grid = ALGORITHMS[alg](grid)
-            results.append([i, grid.tot_len()])
+            results.append([i, grid.tot_len(), lower_bound(distance(grid)), upper_bound(distance(grid))])
         return results
+
+
+def prompt_alg():
+    """
+    Ask user which algorithm should be used.
+    :return: string
+    """
+    algorithms = {0: 'random', 1: 'greedy', 2: 'greedy2', 3: 'hillclimber', 4: 'simulated_annealing'}
+    print("What algorithm should be used (type INFO to get description of algorithms)")
+    print(''.join(['{0}{1}'.format(str(key) + ': ', value + '  ') for key, value in algorithms.items()]), end=' ')
+    user_in = input('\n> ')
+    try:
+        user_in = int(user_in)
+    except ValueError:
+        print('Invalid number, choose one from list below')
+        return prompt_alg()
+    if user_in not in algorithms:
+        print('Invalid algorithm, choose one from list below')
+        return prompt_alg()
+    else:
+        return algorithms[user_in]
+
+
+def choose_distr():
+    """
+    Ask user which district should be calculated.
+    :return:
+    """
+    print("District to solve:\n[1] [2] [3]")
+    user_in = input('> ')
+    try:
+        user_in = int(user_in)
+    except ValueError:
+        print('Not a valid number, choose from numbers below')
+        return choose_distr()
+    if user_in <= 3:
+        return user_in
+    else:
+        return choose_distr()
